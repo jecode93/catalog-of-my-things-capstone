475b4b214df5b3f857435b5ef9ff3531f4a4f013
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-10-17 11:35:32 +0100

    Update game.rb

--- classes/game.rb
+++ classes/game.rb
@@ -9,7 +9,7 @@ class Game < Item
     @multiplayer = multiplayer,
                    @last_played_at = last_played_at
   end
- 
+
   def can_be_archived?
     super && (Time.now - @last_played_at) > 2 * 365 * 24 * 60 * 60
   end
