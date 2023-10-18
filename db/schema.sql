095ff0e9934bd43c3b01eeebc9522ae4a39775b1
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-10-18 14:40:04 +0100

    add game table

--- db/schema.sql
+++ db/schema.sql
@@ -14,5 +14,21 @@ CREATE TABLE
         color varchar(50)
     )
 
+    CREATE TABLE
+    game(
+        id SERIAL primary key,
+				publish_date DATE,
+        multiplayer VARCHAR(50),
+        last_played_at DATE,
+        Item_id INTEGER REFERENCES item(id),
+    )
+ CREATE TABLE
+    author(
+        id SERIAL primary key,
+				first_name VARCHAR(50),
+        last_name VARCHAR(50),
+    )
 CREATE INDEX books_id ON books(id);
 CREATE INDEX item_label_id ON item(id);
+CREATE INDEX author_id ON author(id);
+CREATE INDEX item_game_id ON item(id);
