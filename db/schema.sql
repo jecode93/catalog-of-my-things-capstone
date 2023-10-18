a0269ff8add319668c980a1ddd290880249ff447
katarighe <m.ighe@hotmail.com>
2023-10-18 12:34:49 +0300

    Create SQL schema

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,26 @@ CREATE TABLE
 
 CREATE INDEX books_id ON books(id);
 CREATE INDEX item_label_id ON item(id);
+
+-- Create Music Albums table
+CREATE TABLE IF NOT EXISTS music_albums (
+  id INT GENERATED ALWAYS AS IDENTITY
+  on_spotify BOOLEAN NOT NULL,
+  genre VARCHAR(100) REFERENCES genres(name),
+  author VARCHAR(100) REFERENCES authors(first_name),
+  label VARCHAR(100) REFERENCES labels(title),
+  publish_date DATE,
+  archived BOOLEAN,
+  PRIMARY KEY(id)
+);
+
+-- Create Genres table
+CREATE TABLE IF NOT EXISTS genres (
+  id INT GENERATED ALWAYS AS IDENTITY
+  name VARCHAR(100),
+  items INT REFERENCES (books, music_albums, games),
+  PRIMARY KEY(id, items)
+);
+
+CREATE INDEX music_albums_id ON music_albums(id);
+CREATE INDEX genre_id ON genre(id);
