4d548e6a0fd6fc06320754077b01c2606d29ce36
katarighe <m.ighe@hotmail.com>
2023-10-18 11:32:02 +0300

    Update the MusicAlbum class according to the test file and errors

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -2,15 +2,13 @@ require_relative 'item'
 require 'date'
 
 class MusicAlbum < Item
-  attr_accessor :id, :on_spotify
+  attr_reader :id, :on_spotify
 
-  def initialize(on_spotify, archived, publish_date)
+  def initialize(archived, publish_date, on_spotify)
     super(archived, publish_date)
     @id = Random.rand(1...1000)
     @on_spotify = on_spotify
   end
-
-  private
 
   def can_be_archived?
     super == true && @on_spotify == true
