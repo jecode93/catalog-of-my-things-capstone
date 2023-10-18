fe8caf321d47b9616a820ac72093406dc59bdf2b
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-10-18 14:40:44 +0100

    Update create.rb

--- utils/create.rb
+++ utils/create.rb
@@ -1,5 +1,6 @@
 require_relative '../classes/book'
 require_relative '../classes/item'
+require_relative '../classes/game'
 require_relative '../utils/preserve_data'
 
 module Create
@@ -16,5 +17,21 @@ module Create
     @books << Book.new(publisher, cover_state, publish_date)
     write_to_file(@books, './data/books.json')
     puts "Book '#{publisher}' created successfully'"
+  end
+
+  def create_game
+    @game = []
+    print 'Multiplayer: '
+    multiplayer = gets.chomp.to_s
+
+    print 'Last played at: '
+    last_played_at = gets.chomp.to_s
+
+    print 'Publish date: '
+    publish_date = gets.chomp.to_s
+
+    @game << Game.new(multiplayer, last_played_at, publish_date.to_s)
+    write_to_file(@game, './data/game.json')
+    puts "Game '#{multiplayer}' created successfully"
   end
 end
