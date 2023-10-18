02c1c0453d611f81929ccf47186baf591416abc5
katarighe <m.ighe@hotmail.com>
2023-10-18 11:07:33 +0300

    Update the details, such as id

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,16 +1,18 @@
 require_relative 'item'
+require 'date'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify
+  attr_accessor :id, :on_spotify
 
-  def initialize(on_spotify, publish_date)
-    super(publish_date)
+  def initialize(on_spotify, archived, publish_date)
+    super(archived, publish_date)
+    @id = Random.rand(1...1000)
     @on_spotify = on_spotify
   end
 
   private
 
   def can_be_archived?
-    super && true
+    super == true && @on_spotify == true
   end
 end
