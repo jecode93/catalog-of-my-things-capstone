c2f6f3cd81ac64529e42367488c5065408ced6d2
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-10-18 14:41:12 +0100

    add game to list

--- utils/list.rb
+++ utils/list.rb
@@ -1,6 +1,7 @@
 require_relative '../classes/book'
 require_relative '../classes/item'
 require_relative '../classes/label'
+require_relative '../classes/game'
 require_relative 'preserve_data'
 
 module List
@@ -17,6 +18,22 @@ module List
     puts 'Labels list is empty' if @labels.empty?
     @labels.each_with_index do |label, i|
       puts "#{i}) Title: '#{label['title']}', Color: '#{label['color']}', Items: '#{label['items']}'"
+    end
+  end
+
+  def list_authors
+    @authors = read_from_file('./data/author.json')
+    puts 'Authors list is empty' if @authors.empty?
+    @authors.each_with_index do |author, i|
+      puts "#{i}) First Name: '#{author['first_name']}', Last Name: '#{author['last_name']}', Items: '#{author['items']}'"
+    end
+  end
+
+  def list_games
+    @games = read_from_file('./data/game.json')
+    puts 'Game list is empty' if @games.empty?
+    @games.each_with_index do |game, i|
+      puts "#{i}) Multiplayer: #{game['multiplayer']}, Last played at: #{game['last_played_at']}, Publish date: #{game['publish_date']}"
     end
   end
 end
